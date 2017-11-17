aa9203d52018352c9d646e6184bdb0234e343058
Kyle Nally <kylegnally@gmail.com>
2017-11-17 18:28:57 -0500

    Create Recipe class. Create Recipe class singleton. Create Ingredient class. Create class-level vars for Ingredient class.

---
+++ app/src/main/java/com/knally0045/mykitchencounter/Recipe.java
@@ -0,0 +1,29 @@
+package com.knally0045.mykitchencounter;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kyleg on 11/17/2017.
+ */
+
+public class Recipe {
+
+    private static Recipe sRecipe;
+
+    private ArrayList<Ingredient> mIngredients;
+
+    public static Recipe get (Context context) {
+        if (sRecipe == null) {
+            sRecipe = new Recipe(context);
+        }
+
+        return sRecipe;
+    }
+
+    private Recipe(Context context) {
+        mIngredients = new ArrayList<>();
+    }
+
+}
