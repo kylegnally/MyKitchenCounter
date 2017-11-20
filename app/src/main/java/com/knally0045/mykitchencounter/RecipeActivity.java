15183aaffc67ff5f055f1641b11bf8f006e43b3c
Kyle Nally <kylegnally@gmail.com>
2017-11-20 08:26:52 -0500

    Add FragmentManager to RecipeActivity.java for main menu screen.

--- app/src/main/java/com/knally0045/mykitchencounter/RecipeActivity.java
+++ app/src/main/java/com/knally0045/mykitchencounter/RecipeActivity.java
@@ -1,6 +1,8 @@
 package com.knally0045.mykitchencounter;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class RecipeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new RecipeMenuFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
---
+++ app/src/main/java/com/knally0045/mykitchencounter/RecipeMenuFragment.java
@@ -0,0 +1,55 @@
+package com.knally0045.mykitchencounter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by kyleg on 11/20/2017.
+ */
+
+public class RecipeMenuFragment extends Fragment {
+
+    private ImageButton mNewRecipeButton;
+    private ImageButton mBrowseSavedRecipesButton;
+    private TextView mNewRecipeTextView;
+    private TextView mBrowseRecipesTextView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        mNewRecipeButton = (ImageButton) v.findViewById(R.id.new_recipe_button);
+        mNewRecipeTextView = (TextView) v.findViewById(R.id.new_recipe_text);
+        mNewRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBrowseSavedRecipesButton = (ImageButton) v.findViewById(R.id.recipe_book_button);
+        mBrowseRecipesTextView = (TextView) v.findViewById(R.id.browse_recipe_text);
+        mBrowseSavedRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return v;
+    }
+}
