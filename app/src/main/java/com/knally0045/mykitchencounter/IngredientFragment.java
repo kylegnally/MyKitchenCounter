d76e96792480e0b51cc3ced77f8c6aca6d96803b
Kyle Nally <kylegnally@gmail.com>
2017-11-26 12:46:28 -0500

    Add new LinearLayout to existing fragment on button click. TODO: fix positioning. TODO: allow newly-created ImageButton to add new LinearLayout to fragment.

--- app/src/main/java/com/knally0045/mykitchencounter/IngredientFragment.java
+++ app/src/main/java/com/knally0045/mykitchencounter/IngredientFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 /**
  * Created by kyleg on 11/23/2017.
@@ -21,6 +22,7 @@ public class IngredientFragment extends Fragment {
     private ImageButton mAddIngredientButton;
     private EditText mIngredientString;
     private RecyclerView mIngredientRecyclerView;
+    private LinearLayout mLayoutToAdd;
     private Recipe mRecipe;
     private Context mContext;
 
@@ -34,10 +36,21 @@ public class IngredientFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.recipe_ingredient_fragment, container, false);
+
+        mLayoutToAdd = (LinearLayout) v.findViewById(R.id.layout_new_ingredient);
         mIngredientString = (EditText) v.findViewById(R.id.new_ingredient);
         // add OnTextChanged listener here
 
         mAddIngredientButton = (ImageButton) v.findViewById(R.id.add_ingredient_button);
+        mAddIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater newInflater = LayoutInflater.from(getContext());
+                View view1 = newInflater.inflate(R.layout.recipe_ingredient_fragment, null);
+                mLayoutToAdd.addView(view1);
+            }
+        });
+
         // add onClick listener here
 
 //        mIngredientRecyclerView = (RecyclerView) v.findViewById(R.id.ingredient_recycler_view);
